c3374be16821f91c83651b2df0b901a1aaef585e
okwongbassey <skip.bassey@duke-energy.com>
2019-11-26 21:34:25 -0500

    removed home comp

--- wholesale-tracker/src/app/app.module.ts
+++ wholesale-tracker/src/app/app.module.ts
@@ -7,14 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 // Modules
 import { MaterialModule } from './material.module';
-import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent,
     DashboardComponent,
   ],
   imports: [
