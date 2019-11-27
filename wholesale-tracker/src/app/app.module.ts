b8cce80cc2b817d2840e0f9dbb14679ce0f09d02
okwongbassey <skip.bassey@duke-energy.com>
2019-11-26 21:32:46 -0500

    created dashboard comp

--- wholesale-tracker/src/app/app.module.ts
+++ wholesale-tracker/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 // Modules
 import { MaterialModule } from './material.module';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
