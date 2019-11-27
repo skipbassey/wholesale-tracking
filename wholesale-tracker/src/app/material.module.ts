983baf09f2f1537ea870c0951673f687bd7519ea
okwongbassey <skip.bassey@duke-energy.com>
2019-11-26 20:43:05 -0500

    created material module

---
+++ wholesale-tracker/src/app/material.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    MatToolbarModule
+  ],
+  imports: [
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class MaterialModule { }
