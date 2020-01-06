4905320d80f46811a19e71a234d2d787d11d8290
okwongbassey <skip.bassey@duke-energy.com>
2020-01-06 09:26:21 -0500

    added leadd service to app module

--- wholesale-tracker/src/app/app.module.ts
+++ wholesale-tracker/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 // Modules
 import { MaterialModule } from './material.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LeadService } from 'src/services/lead.service';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserAnimationsModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [
+    LeadService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
